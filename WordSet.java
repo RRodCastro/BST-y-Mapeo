5501d6e02b8cfd7c22edfaa3805f82284d7d6727
RRodCastro <mrcastro9574@gmail.com>
2015-11-09 16:24:32 -0600

    Create WordSet.java

---
+++ WordSet.java
@@ -0,0 +1,17 @@
+/*
+ * /* Programa, obtenido de blackboard, el cual fue subido como referencia por catedratico 
+ * Integrantes: Rodrigo Castro #14092
+ * Integrantes: Hugo Noriega #14097 
+UVG
+Algoritmos y Estructuras de Datos - 2011
+Hoja de trabajo 7 
+Autor: Eduardo Castellanos
+
+Descripción: WordSet. Interfaz para las implementaciones de clases que almacenarán palabras.
+*/
+public interface WordSet {
+
+	public void add(Word wordObject);
+	public Word get(Word word);
+	
+}
